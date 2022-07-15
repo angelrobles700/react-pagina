b94f31115145aab2d5eb664739e1ffeec71a4191
angelrobles700 <ROVM201922866@tese.edu.mx>
2022-07-15 13:54:25 -0500

    Updates

---
+++ precache-manifest.f8e8a0f91535eab66b32f7ddd212b4aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee1b1ce2bc0e89a1c9235d4f51f51cf",
+    "url": "/react-pagina/index.html"
+  },
+  {
+    "revision": "ffef24f4b22093f70b14",
+    "url": "/react-pagina/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "10f6d5040d25d17ded71",
+    "url": "/react-pagina/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ffef24f4b22093f70b14",
+    "url": "/react-pagina/static/js/2.92cd5f58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pagina/static/js/2.92cd5f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f6d5040d25d17ded71",
+    "url": "/react-pagina/static/js/main.4f1597df.chunk.js"
+  },
+  {
+    "revision": "ea615c16ec67cea91d38",
+    "url": "/react-pagina/static/js/runtime-main.93510095.js"
+  }
+]);
